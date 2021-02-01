d0d443ca594db07779fffe97dfdb28301ee8e869
Ariel-Rivas <irbac5@gmail.com>
2021-02-01 15:44:34 -0400

    trying something in gitignore

--- efe/main.cpp
+++ efe/main.cpp
@@ -1,3 +1,37 @@
-stonks con mayonesa y un buen sprite :v
+#include <bits/stdc++.h>
 
-este es un cambio
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double dbl;
+#define QuieroComer ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FileIn freopen("input.txt", "r", stdin)
+#define FileOut freopen("output.txt", "w", stdout)
+#define endl "\n"
+#define debug(x) cout<<"--> "<<x<<endl
+int const MOD = 10e9+7;
+
+int main()
+{
+    //esto no se deberia poder ver
+    return 0;
+}
+
+
+/*--------------------------------------------/
+	* overflow?
+	* Corner cases (n=1? || n=n-1?)
+	* n=n+1 (!=) n+=1; 
+	  (copy n) ~ (don't make a copy of n)
+	* if(use vector, string, deque in function) use "&"before.
+	  (to rewrite, instead of make a copy)
+	* DON'T GET STUCK ON ONE APPROACH
+	
+	//Animo, sales de gris :'v, haz upsolving cuando termine >:v
+	//Despues comes :3
+----------------------------------------------/
+*/
+
+
+
